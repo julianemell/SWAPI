adbd6fb8f0ef1369201d5474eecacd9fdff1645a
Julia Nemell <jnemell@gmail.com>
2022-05-02 15:15:09 +0200

    added links to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
 
 
 const HomePage = () => {
 	return (
 		<div>
-			<p>Welcome to the home page</p>
+			<h1>Star Wars information...</h1>
+			<div className="home-container">
+				<Link to="/films">
+					Go to all Star Wars films
+				</Link>
+				<Link to="/people">
+					Check out all characters
+				</Link>
+			</div>
 		</div>
 	)
 }
